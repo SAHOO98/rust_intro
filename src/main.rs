7f6fcc114ee1ec81363ceb812c75bfa33cbf8c32
Saptarshi Sahoo <uzumaki.sahoo@gmail.com>
2021-05-07 17:09:48 +0530

    Added a separator between each function calls

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ mod var;
 mod data_types;
 fn main() {
     print::run();
+    println!("\n=================================\n");
     var::run();
+    println!("\n=================================\n");
     data_types::run();
+    println!("\n=================================\n");
 }   
